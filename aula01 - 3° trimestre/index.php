382b110834ff88a9ecdd23bb41a4788e75bd3130
Isabella <isabella.avrodrigues@aluno.edu.es.gov.br>
2025-09-03 11:56:42 -0300

    Create index.php

---
+++ aula01 - 3° trimestre/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> Aula 01 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo " Hello World! ";
+            echo " Isabella Alves"; 
+
+        ?>
+    </body>
+</html> 
