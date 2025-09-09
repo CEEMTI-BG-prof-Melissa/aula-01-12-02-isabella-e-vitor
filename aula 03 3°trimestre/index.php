043e6f2ddd818fbcbfc9a38323729c874d2ffa64
Isabella <isabella.avrodrigues@aluno.edu.es.gov.br>
2025-09-09 11:55:25 -0300

    Create index.php

---
+++ aula 03 3°trimestre/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title> Aula 03 PHP </title>
+    </head>
+    <body>
+    <?php
+        $nome="Isabella Alves";
+
+        $numero1= 10;
+        $numero2= 2;
+
+        $soma= $numero1+$numero2;
+
+        echo "Meu nome é" .$nome; 
+        echo " <br> Meu nome é $nome";
+        echo " <br> <br> A soma de $numero1 e $numero2 é: $soma";
+    ?> 
+    </body>
+</html>
